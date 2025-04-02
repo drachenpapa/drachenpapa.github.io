eaf5865e07b1a95172a46dc763777470eeb608e9
Henning Steinberg <henning.steinberg@hmmh.de>
2025-04-02 16:33:32 +0200

    add burger menu for small screens

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    lucide.createIcons();
-    initSlider();
+    initNavMenu();
     initializeTheme();
+    initSlider();
+    lucide.createIcons();
 });
 
-function toggleTheme() {
-    document.documentElement.classList.toggle("dark");
+function initNavMenu() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("hidden");
+        navMenu.classList.toggle("flex");
+    });
 }
 
 function initializeTheme() {
@@ -13,31 +20,33 @@ function initializeTheme() {
     document.getElementById("toggle").checked = document.documentElement.classList.contains("dark");
 }
 
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
 function initSlider() {
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
     const dotsContainer = document.querySelector('.dots');
+    const dots = [];
 
     slides.forEach((_, index) => {
-        const dot = createDot(index);
-        dotsContainer.appendChild(dot);
-    });
-
-    function createDot(index) {
         const dot = document.createElement('div');
         dot.className = 'dot';
         if (index === 0) dot.classList.add('active');
+
         dot.addEventListener('click', () => moveToSlide(index));
-        return dot;
-    }
+        dotsContainer.appendChild(dot);
+        dots.push(dot);
+    });
 
     function moveToSlide(index) {
         slider.style.transform = `translateX(-${index * 100}%)`;
-        document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
-        dotsContainer.children[index].classList.add('active');
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[index].classList.add('active');
     }
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+function toggleTheme() {
+    document.documentElement.classList.toggle("dark");
 }
